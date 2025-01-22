57b8fc6e4ecbebf3dd16923a14fa7d6763a6ba39
ssmdu <soni.sakthi@gmail.com>
2025-01-22 09:43:52 +0000

    updated css  with scss

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ add_action(
  * Enqueue theme styles
  */
 function test_theme() {
-    wp_enqueue_style( 'style', get_template_directory_uri() . '/custom.css');
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/css/theme.css');
  }
  add_action( 'wp_enqueue_scripts', 'test_theme' );
 
